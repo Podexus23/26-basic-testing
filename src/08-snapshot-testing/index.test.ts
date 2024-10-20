e70de5f16b9800e39b7ba83011b9c630f1aa9028
Podexus23 <santon23@gmail.com>
2024-10-20 13:09:06 +0300

    feat: 8 ll done

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,33 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const elems = [1, 2, 3];
+    const list = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    };
+
+    const res = generateLinkedList(elems);
+    expect(res).toStrictEqual(list);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const elems = [4, 5, 6];
+    const res = generateLinkedList(elems);
+
+    expect(res).toMatchSnapshot();
   });
 });
